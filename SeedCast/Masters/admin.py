e37cf575c361a456eb0cac20f5cf1337a69091b2
Sai-Kumar-PRACS <32595002+Sai-Kumar-PRACS@users.noreply.github.com>
2018-03-12 22:11:30 +0530

    Update admin.py

--- SeedCast/Masters/admin.py
+++ SeedCast/Masters/admin.py
@@ -231,30 +231,30 @@ admin.site.register(Villages, VillagesAdmin)
 
 
 #
-# class VAWDemandResource(resources.ModelResource):
-#     class Meta:
-#         model = VAWDemand
-#         fields = ('vaw', 'varietyName', 'quantity',)
-#
-#
-# class VAWDemandAdmin(ModelAdmin):
-#     list_display = ('vaw', 'varietyName', 'quantity', 'date_collected',)
-#     resource_class = VAWDemandResource
-#
-# admin.site.register(VAWDemand, VAWDemandAdmin)
-#
-# #Dealer Demand
-# class DealerDemandResource(resources.ModelResource):
-#     class Meta:
-#         model = DealerDemand
-#         fields = ('dealer', 'variety_name', 'quantity', 'date_collected', 'chk')
-#
-#
-# class DealerDemandAdmin(ModelAdmin):
-#     list_display = ('dealer', 'variety_name', 'quantity', 'date_collected',)
-#     resource_class = DealerDemandResource
-#
-# admin.site.register(DealerDemand, DealerDemandAdmin)
+class VAWDemandResource(resources.ModelResource):
+    class Meta:
+        model = VAWDemand
+        fields = ('vaw', 'varietyName', 'quantity',)
+
+
+class VAWDemandAdmin(ModelAdmin):
+    list_display = ('vaw', 'varietyName', 'quantity', 'date_collected',)
+    resource_class = VAWDemandResource
+
+admin.site.register(VAWDemand, VAWDemandAdmin)
+
+#Dealer Demand
+class DealerDemandResource(resources.ModelResource):
+    class Meta:
+        model = DealerDemand
+        fields = ('dealer', 'variety_name', 'quantity', 'date_collected', 'chk')
+
+
+class DealerDemandAdmin(ModelAdmin):
+    list_display = ('dealer', 'variety_name', 'quantity', 'date_collected',)
+    resource_class = DealerDemandResource
+
+admin.site.register(DealerDemand, DealerDemandAdmin)
 
 
 
